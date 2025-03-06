95bfc6a31c774ce8774cae073f87e85f4533361c
Rr728 <rrm69114@gmail.com>
2025-03-06 10:56:06 +0530

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Constructor
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        // DbSet properties
+        public DbSet<User> Users { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+    }
+}
