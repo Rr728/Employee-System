e7419197c498e5f1543b64e36d348f23474800e8
Rr728 <rrm69114@gmail.com>
2025-03-06 11:03:04 +0530

    Add files via upload

---
+++ ViewModels/DepartmentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApplication2.Models;
+using WebApplication2.ViewModels;
+
+namespace WebApplication2.ViewModels
+{
+    public class DepartmentViewModel
+    {
+        public int DepartmentId { get; set; }
+
+        [Required(ErrorMessage = "Department name is required.")]
+        public string Name { get; set; }
+    }
+}
+
---
+++ ViewModels/LeaveRequestViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.ViewModels
+{
+    public class LeaveRequestViewModel
+    {
+        public int LeaveId { get; set; }
+
+        [Required(ErrorMessage = "Employee is required.")]
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "Leave type is required.")]
+        public string LeaveType { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Start date is required.")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "End date is required.")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [Required(ErrorMessage = "Reason is required.")]
+        public string Reason { get; set; } = string.Empty;
+
+        public string Status { get; set; } = "Pending";
+    }
+}
